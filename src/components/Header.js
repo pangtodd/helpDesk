d3a393d91d40b89097b78170b2b6894bf883d847
Todd Pangilinan <pang.todd@gmail.com>
2022-08-15 22:30:46 -0700

    updated project to work with Redux.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import helpImage from "./../img/help.jpeg";
+import helpImage from "./../img/HelpDesky.png";
 
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
-      <img src={helpImage} alt="An image of legos" />
+      <img src={helpImage} alt="An illustration of different people in different primary colors" />
     </React.Fragment>
   );
 }
